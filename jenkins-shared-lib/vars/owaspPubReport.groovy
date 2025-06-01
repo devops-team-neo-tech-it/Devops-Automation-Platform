987f388d34607ccc4ec018afa5428f1bee841207
Abduh-Belhaje <abdelkodoussebelhaje@gmail.com>
2025-06-01 15:03:11 +0100

    update the func

--- jenkins-shared-lib/vars/owaspPubReport.groovy
+++ jenkins-shared-lib/vars/owaspPubReport.groovy
@@ -1,14 +1,12 @@
-def call(String projectName){
+def call(String projectName) {
     stage('Publish Report') {
-        steps {
-            publishHTML (target: [
-                allowMissing: false,
-                alwaysLinkToLastBuild: true,
-                keepAll: true,
-                reportDir: 'reports',
-                reportFiles: "${projectName}-report.html",
-                reportName: "OWASP Dependency-Check - ${projectName}"
-            ])
-        }
+        publishHTML target: [
+            allowMissing: false,
+            alwaysLinkToLastBuild: true,
+            keepAll: true,
+            reportDir: 'reports',
+            reportFiles: "${projectName}-report.html",
+            reportName: "OWASP Dependency-Check - ${projectName}"
+        ]
     }
 }
