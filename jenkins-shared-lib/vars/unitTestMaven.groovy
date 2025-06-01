f7c4e6e83cb130d94a51da92fc2b7a47d171df55
Abduh-Belhaje <abdelkodoussebelhaje@gmail.com>
2025-06-01 16:40:55 +0100

    add exception handling to the func

--- jenkins-shared-lib/vars/unitTestMaven.groovy
+++ jenkins-shared-lib/vars/unitTestMaven.groovy
@@ -1,3 +1,10 @@
 def call(String projectPath){
-    sh "cd ${projectPath} && mvn test"
+    try {
+        echo '🧪 Running Unit Tests with Maven'
+        sh "cd ${projectPath} && mvn test"
+        echo '✅ Unit Tests completed successfully'
+    } catch (Exception e) {
+        env.FAILURE_REASON = 'unit_tests_failed'
+        error("Unit tests failed: ${e.message}")
+    }
 }
