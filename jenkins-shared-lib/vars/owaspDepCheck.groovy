12e8ea47a67cb12c4bc49d6edacd75842e37cf40
Abduh-Belhaje <abdelkodoussebelhaje@gmail.com>
2025-06-01 14:46:39 +0100

    use owasp check jenkins tool

--- jenkins-shared-lib/vars/owaspDepCheck.groovy
+++ jenkins-shared-lib/vars/owaspDepCheck.groovy
@@ -1,13 +1,11 @@
-def call(String projectName, String projectPath, String nvdApiKey) {
+def call(String odc, String projectName, String projectPath, String nvdApiKey) {
     try {
+        dependencyCheck odcInstallation: odc,
+                        additionalArguments: "--scan ${projectPath} --format HTML --out ./reports --nvdApiKey ${nvdApiKey}"
+
         sh """
-            dependency-check.sh \\
-                --project "${projectName}" \\
-                --scan "${projectPath}" \\
-                --format "HTML" \\
-                --out ./reports \\
-                --nvdApiKey "${nvdApiKey}" && \
-            mv ./reports/dependency-check-report.html ./reports/${projectName}-report.html
+            mkdir -p reports
+            mv dependency-check-report.html reports/${projectName}-report.html
         """
     } catch (Exception e) {
         env.FAILURE_REASON = 'owasp_dependency_check_failed'
