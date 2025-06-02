a94e339b0f6fcfce16d8a15f47cd4b936ee02956
Abduh-Belhaje <abdelkodoussebelhaje@gmail.com>
2025-06-02 10:36:27 +0100

    use the global exception handling

--- jenkins-shared-lib/vars/unitTestMaven.groovy
+++ jenkins-shared-lib/vars/unitTestMaven.groovy
@@ -1,10 +1,5 @@
 def call(String projectPath){
-    try {
-        echo '🧪 Running Unit Tests with Maven'
-        sh "cd ${projectPath} && mvn test"
-        echo '✅ Unit Tests completed successfully'
-    } catch (Exception e) {
-        env.FAILURE_REASON = 'unit_tests_failed'
-        error("Unit tests failed: ${e.message}")
-    }
+    echo '🧪 Running Unit Tests with Maven'
+    sh "cd ${projectPath} && mvn test"
+    echo '✅ Unit Tests completed successfully'
 }
