997d8b7deba39dea351febc518e67dbda5d56ae9
Abduh-Belhaje <abdelkodoussebelhaje@gmail.com>
2025-06-02 09:18:14 +0100

    update quality gates func

--- jenkins-shared-lib/vars/sonarQualityGates.groovy
+++ jenkins-shared-lib/vars/sonarQualityGates.groovy
@@ -1,16 +1,12 @@
-def call(){
+def call(String FAILURE_REASON){
     stage('SonarQube Quality Gates') {
-        steps {
-            script {
-                try {
-                    timeout(time: 10, unit: 'MINUTES') {
-                        waitForQualityGate abortPipeline: true
-                    }
-                } catch (Exception e) {
-                    FAILURE_REASON = 'sonarqube_quality_gate_failed'
-                    error("SonarQube quality gate failed: ${e.message}")
-                }
+        try {
+            timeout(time: 10, unit: 'MINUTES') {
+                waitForQualityGate abortPipeline: true
             }
+        } catch (Exception e) {
+            FAILURE_REASON = 'sonarqube_quality_gate_failed'
+            error("SonarQube quality gate failed: ${e.message}")
         }
     }
 }
