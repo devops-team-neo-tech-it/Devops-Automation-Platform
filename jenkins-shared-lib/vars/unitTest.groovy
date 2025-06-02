f18cdc91ac65f9d284d24891a6dd8ac0cf5613fc
Abduh-Belhaje <abdelkodoussebelhaje@gmail.com>
2025-06-02 10:54:52 +0100

    use failure reason as a env var

--- jenkins-shared-lib/vars/unitTest.groovy
+++ jenkins-shared-lib/vars/unitTest.groovy
@@ -1,14 +1,14 @@
-def call(String projectPath, String buildTool){
+def call(String projectPath, String buildTool, def env){
     try{
         switch(buildTool) {
             case "maven":
-                UnitTestMaven(projectPath, buildTool)
+                unitTestMaven(projectPath)
                 break
             default:
                 error("Unsupported build tool: ${buildTool}")
         }
     }catch(Exception e){
-        FAILURE_REASON = 'unit_test_failed'
+        env.FAILURE_REASON = 'unit_test_failed'
         error("Unit Tests failed: ${e.message}")
     }
 }
