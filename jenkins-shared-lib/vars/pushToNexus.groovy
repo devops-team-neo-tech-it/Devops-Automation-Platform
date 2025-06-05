9ad7c253615641a35b0b78bd2649fbde317d800b
Abduh-Belhaje <abdelkodoussebelhaje@gmail.com>
2025-06-05 13:10:31 +0100

    add nexus integration module

---
+++ jenkins-shared-lib/vars/pushToNexus.groovy
@@ -0,0 +1,13 @@
+def call(String BUILD_TOOL, String PROJECT_PATH, String NEXUS_URL, String REPOSITORY) {
+    
+    echo '🚀 Pushing to Nexus Repository'
+    try {
+        switch(BUILD_TOOL) {
+            case 'maven':
+                mavenNexus(PROJECT_PATH, NEXUS_URL, REPOSITORY)
+                break
+        }
+    } catch (Exception e) {
+        error("Failed to push to Nexus: ${e.message}")
+    }
+}
